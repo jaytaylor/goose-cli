f5c297cbc40fe3dfc77b0094db51ebacae226577
Jay Taylor <outtatime@gmail.com>
2018-03-19 23:25:50 -0700

    Hashed out more of cli.

--- cmd/goose-cli.go
+++ cmd/goose-cli.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,12 +9,17 @@ import (
 	"github.com/advancedlogic/GoOse"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
-var Verbose bool
+var (
+	OutputFormat = "html"
+	Verbose      bool
+)
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.PersistentFlags().StringVarP(&OutputFormat, "output", "o", "text", `Output format, one of "json", "html", "text", "yaml"; (default: text)`)
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Activate verbose output")
 }
 
@@ -31,7 +37,7 @@ func errorExit(err interface{}) {
 var rootCmd = &cobra.Command{
 	Use:   "extract",
 	Short: "Disambiguate an article from HTML content",
-	Long:  "Runs GoOse......................................",
+	Long:  "Runs GoOse command-line client",
 	Args:  cobra.MinimumNArgs(1),
 	PreRun: func(_ *cobra.Command, _ []string) {
 		initLogging()
@@ -55,17 +61,61 @@ var rootCmd = &cobra.Command{
 			errorExit(err)
 		}
 
-		log.Infof("Title: %s", article.Title)
-		h, err := article.TopNode.Html()
-		if err != nil {
+		// log.Infof("Title: %s", article.Title)
+		// log.Infof("Publish Date: %s", article.PublishDate)
+		// h, err := article.TopNode.Html()
+		// if err != nil {
+		// 	errorExit(err)
+		// }
+		// log.Infof("HTML: ", h)
+		// log.Infof("%s", )
+		// log.Infof("%s", )
+		// log.Infof("%s", )
+		// log.Infof("%s", )
+
+		if err := emit(article); err != nil {
 			errorExit(err)
 		}
-		log.Infof("HTML: ", h)
-		// log.Infof("%s", )
-		// log.Infof("%s", )
-		// log.Infof("%s", )
-		// log.Infof("%s", )
 	},
+}
+
+func emit(article *goose.Article) error {
+	switch OutputFormat {
+	case "json":
+		bs, err := json.MarshalIndent(*article, "", "    ")
+		if err != nil {
+			return err
+		}
+		fmt.Print(string(bs))
+
+	case "html":
+		// fmt.Printf("%s", article.RawHTML)
+		h, err := article.TopNode.Html()
+		if err != nil {
+			return err
+		}
+		fmt.Printf(h)
+
+	case "text":
+		fmt.Printf("Title: %s\n", article.Title)
+		fmt.Printf("Publish Date: %s\n", article.PublishDate)
+		fmt.Printf("Canonical Link: %s\n", article.CanonicalLink)
+		fmt.Printf("Final URL: %s\n", article.FinalURL)
+		fmt.Printf("Meta Lang: %s\n", article.MetaLang)
+		fmt.Printf("Content:\n%s\n", article.CleanedText)
+
+	case "yaml":
+		bs, err := yaml.Marshal(*article)
+		if err != nil {
+			return err
+		}
+		fmt.Print(string(bs))
+
+	default:
+		return fmt.Errorf("unrecognized output format %q", OutputFormat)
+	}
+
+	return nil
 }
 
 var versionCmd = &cobra.Command{
